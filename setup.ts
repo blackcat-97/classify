875fb8f9974fc3f70794eb3c27c430e2a61f8d84
blackcat-97 <elitedev777@gmail.com>
2023-10-14 08:29:43 -0700

    feat: initial commit

---
+++ setup.ts
@@ -0,0 +1,60 @@
+Sample Portfolios
+https://github.com/kentcdodds
+https://github.com/bchiang7?tab=repositories
+https://github.com/sdras
+https://github.com/andrewwylde
+https://github.com/kevincennis?tab=repositories
+https://github.com/wesbos
+https://github.com/mhevery
+
+https://www.toptal.com/resume/justin-michela
+https://www.toptal.com/resume/nathan-babcock
+https://www.toptal.com/resume/johnathan-hebert
+
+
+
+Useful
+https://mail.tm/en/
+https://www.pubnub.com/blog/nodejs-websocket-programming-examples/
+https://medium.com/swlh/grid-based-movement-in-a-top-down-2d-rpg-with-phaser-3-e3a3486eb2fd
+https://ideogram.ai/
+https://www.artguru.ai/swap-face/
+
+
+Orelly URL
+https://learning.oreilly.com/library/view/web-development-with/9781803230603/B18654_01.xhtml
+https://learning.oreilly.com/library/view/lets-build-a/9781484242490/html/475310_1_En_BookFrontmatter_OnlinePDF.xhtml
+https://www.mapeditor.org/
+https://learning.oreilly.com/library/view/lets-build-a/9781484242490/html/Cover.xhtml
+https://learning.oreilly.com/library/view/artificial-intelligence-programming/9781119820864/navigation.xhtml
+
+-- AI / ML
+https://learning.oreilly.com/library/view/hands-on-machine-learning/9781098125967/
+https://learning.oreilly.com/library/view/generative-deep-learning/9781098134174/
+https://learning.oreilly.com/library/view/deep-reinforcement-learning/9781838826994/
+https://learning.oreilly.com/library/view/artificial-intelligence-programming/9781119820864/
+
+-- Linear Algebra
+https://www.khanacademy.org/math/linear-algebra
+
+
+Funny URLs
+https://hexgl.bkcore.com/play/
+
+
+
+
+English Study
+https://www.betteratenglish.com/category/transcripts
+
+https://movies.fandom.com/wiki/Category:Disney_Transcripts
+https://sfy.ru/scripts/pdf
+https://scriptpdf.com/full-list/
+https://gointothestory.blcklst.com/script-download-links-9313356d361c
+http://www.awesomefilm.com/
+
+https://transcripts.foreverdreaming.org/viewforum.php?f=718#p187520
+
+http://www.script-o-rama.com/movie_scripts/u/up-in-the-air-script-transcript.html
+https://www.scripts.com/script.php?id=up_in_the_air_311&p=2
+
